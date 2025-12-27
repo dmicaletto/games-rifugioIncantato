da644c15063a910ebd7498d9c188ac136b340c39
dmicaletto <d.micaletto@informatica.aci.it>
2025-12-27 18:18:20 +0100

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './app.jsx' // Assicurati che il tuo file principale si chiami App.jsx dentro src
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
