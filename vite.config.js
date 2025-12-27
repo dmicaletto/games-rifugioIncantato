a9443d90ebf942086be85b57da4d35a1fdd1c7a5
dmicaletto <d.micaletto@informatica.aci.it>
2025-12-27 19:20:53 +0100

    Update base URL and clean up comments in vite.config.js
    Updated base URL for GitHub repository and removed unnecessary comments.

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // IMPORTANTE: Sostituisci '/NutriAI-Pro/' con il nome esatto del tuo repository GitHub tra due slash.
-  // Questo risolve i problemi di caricamento degli asset (404).
+  // Sostituisci con il nome ESATTO del tuo repository GitHub
   base: '/games-rifugioIncantato/', 
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-  }
 })
