01af43bfae06dc4d67fe0fd14598f27625acc2a8
dmicaletto <d.micaletto@informatica.aci.it>
2025-12-28 11:06:30 +0100

    Update base path comment in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Sostituisci con il nome ESATTO del tuo repository GitHub
-  base: '/games-rifugioIncantato/', 
+  // IMPORTANTE: Assicurati che questo sia il nome ESATTO del tuo repository
+  base: '/games-rifugioIncantato/',
 })
