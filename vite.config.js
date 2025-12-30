6cd95b674607b52539cb6b3cbfc76b22886dedef
dmicaletto <d.micaletto@informatica.aci.it>
2025-12-30 15:03:29 +0100

    Add package version to Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import packageJson from './package.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   // IMPORTANTE: Assicurati che questo sia il nome ESATTO del tuo repository
   base: '/games-rifugioIncantato/',
+  define: {
+    // Rendiamo la versione accessibile globalmente nell'app
+    'import.meta.env.PACKAGE_VERSION': JSON.stringify(packageJson.version)
+  }
 })
